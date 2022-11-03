0babb8f091c44c9e9087b5af624f44385c1a2c36
linbudu599 <linbudu599@gmail.com>
2022-11-03 10:12:29 +0800

    chore: ts scripts

--- packages/with-typescript/index.ts
+++ packages/with-typescript/index.ts
@@ -1,1 +1,11 @@
 /// <reference lib="decorators" />
+
+function ClassDeco(): ClassDecoratorFunction {
+  return (Target, context) => {
+    context.addInitializer(() => {
+      console.log("initializer");
+    });
+  };
+}
+@ClassDeco()
+class Foo {}
